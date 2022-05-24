43e52501c75aaf09042a4c8512dc1afff58b86f3
IT20382094 <navodajayamini567@gmail.com>
2022-05-24 12:22:37 +0530

    components making

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ItemViewComponent } from './item-view/item-view.component';
+import { CartViewComponent } from './cart-view/cart-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    NavigationComponent,
+    ItemViewComponent,
+    CartViewComponent
   ],
   imports: [
     BrowserModule,
